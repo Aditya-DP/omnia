3278e31d856ff1ebf1a7ab2072e679acab388509
abhishek-sa1 <abhishek.sa3@dell.com>
2023-12-19 10:38:31 +0530

    ubuntu provision changes
    Signed-off-by: abhishek-sa1 <abhishek.sa3@dell.com>

--- prereq.sh
+++ prereq.sh
@@ -5,6 +5,7 @@
 
 default_py_version="3.9"
 validate_rocky_os="$(cat /etc/os-release | grep 'ID="rocky"' | wc -l)"
+validate_ubuntu_os="$(cat /etc/os-release | grep 'ID=ubuntu' | wc -l)"
 
 sys_py_version="$(python3 --version)"
 echo "System Python version: $sys_py_version"
@@ -17,23 +18,36 @@ then
  dnf install epel-release -y
 fi
 
-if [[ $(echo $sys_py_version | grep "3.9" | wc -l) != "1" || $(echo $sys_py_version | grep "Python" | wc -l) != "1" ]];
+if [[ "$validate_ubuntu_os" == "1" ]];
 then
- echo "----------------------"
- echo "INSTALLING PYTHON 3.9:"
- echo "----------------------"
- dnf install python39 -y
+    apt-add-repository ppa:deadsnakes/ppa -y
+    echo "----------------------"
+    echo "INSTALLING PYTHON 3.9:"
+    echo "----------------------"
+    apt install python3.9* python3-pip -y
+    echo "--------------"
+    echo "UPGRADING PIP:"
+    echo "--------------"
+    python3.9 -m pip install --upgrade pip
+else
+    if [[ $(echo $sys_py_version | grep "3.9" | wc -l) != "1" || $(echo $sys_py_version | grep "Python" | wc -l) != "1" ]];
+    then
+    echo "----------------------"
+    echo "INSTALLING PYTHON 3.9:"
+    echo "----------------------"
+    dnf install python39 -y
+    fi
+    echo "--------------"
+    echo "UPGRADING PIP:"
+    echo "--------------"
+    python3.9 -m pip install --upgrade pip
 fi
-echo "--------------"
-echo "UPGRADING PIP:"
-echo "--------------"
-python3.9 -m pip install --upgrade pip
 echo "-------------------"
 echo "INSTALLING ANSIBLE:"
 echo "-------------------"
 
 installed_ansible_version=$( ansible --version 2>/dev/null | grep -oP 'ansible \[core \K\d+\.\d+\.\d+' | sed 's/]//')
-target_ansible_version="2.14.12"
+target_ansible_version="2.14.13"
 
 if [[ ! -z "$installed_ansible_version" && "$(echo -e "$installed_ansible_version\n$target_ansible_version" | sort -V | tail -n1)" != "$target_ansible_version" ]];
 then
@@ -46,20 +60,25 @@ then
     echo "Warning: prereq.sh is uninstalling the existing Ansible ($installed_ansible_version) and installing the $target_ansible_version"
 fi
 
-python3.9 -m pip install ansible==7.7.0
+python3.9 -m pip install ansible==7.7.0 cryptography==41.0.7
 python3.9 -m pip install jinja2==3.1.2
 
-dnf install git-lfs -y
-git lfs pull
-
-selinux_count="$(grep "^SELINUX=disabled" /etc/selinux/config | wc -l)"
-if [[ $selinux_count == 0 ]];
+if [[ "$validate_ubuntu_os" == "1" ]];
 then
- echo "------------------"
- echo "DISABLING SELINUX:"
- echo "------------------"
- sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
- echo "SELinux is disabled. Reboot system to notice the change in status before executing playbooks in control plane!!"
+    apt install git -y
+else
+    dnf install git-lfs -y
+    git lfs pull
+
+    selinux_count="$(grep "^SELINUX=disabled" /etc/selinux/config | wc -l)"
+    if [[ $selinux_count == 0 ]];
+    then
+    echo "------------------"
+    echo "DISABLING SELINUX:"
+    echo "------------------"
+    sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+    echo "SELinux is disabled. Reboot system to notice the change in status before executing playbooks in control plane!!"
+    fi
 fi
 echo ""
 echo ""
@@ -73,4 +92,3 @@ echo ""
 echo "Once IP and hostname is set, provide inputs in input/provision_config.yml and execute the playbook provision/provision.yml"
 echo ""
 echo "For more information: https://omnia-doc.readthedocs.io/en/latest/InstallationGuides/InstallingProvisionTool/index.html"
-
